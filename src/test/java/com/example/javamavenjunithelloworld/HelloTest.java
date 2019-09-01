a58efc89774d003712843f93b5589dd8835670e7
venkat-krishna-t <53108167+venkat-krishna-t@users.noreply.github.com>
2019-09-01 08:09:51 +0530

    V3

--- src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
@@ -24,7 +24,7 @@ public class HelloTest {
         Hello hi = new Hello();
         hi.sayHello(stream);
         //assertThat(os.toString(), is(equalTo(Hello.HELLO + "\n")));
-        assertThat(toString(), is(equalTo(Hello.HELLO + "\n")));
+        assertThat(os.toString(), is(equalTo(Hello.HELLO + "\r\n")));
     }
 
     @Test
@@ -37,9 +37,9 @@ public class HelloTest {
         hi.sayHello(stream);
 
         // Does it say "Hello!" three times?
-        String goal = Hello.HELLO + "\n" + Hello.HELLO + "\n" + Hello.HELLO + "\n";
+        String goal = Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n";
        // assertThat(os.toString(), is(equalTo(goal)));
-        assertThat(toString(), is(equalTo(goal)));
+        assertThat(os.toString(), is(equalTo(goal)));
     }
 
     @Test(expected = IllegalArgumentException.class)
